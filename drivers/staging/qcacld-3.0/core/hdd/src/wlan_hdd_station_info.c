b9e0f3261a6d7321a053ae0cd4aef95510ae8f14
ElectroPerf <kunmun.devroms@gmail.com>
2021-03-11 13:54:53 +0000

    Revert "staging: qcacld-3.0: disable more debuging"
    This reverts commit 04388d673532fa978322ad3bf01a0be7abe547c1.

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_station_info.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_station_info.c
@@ -163,13 +163,12 @@ hdd_get_sta_policy[QCA_WLAN_VENDOR_ATTR_GET_STA_INFO_MAX + 1] = {
 static int hdd_get_sta_congestion(struct hdd_adapter *adapter,
 				  uint32_t *congestion)
 {
-	struct cca_stats cca_stats;
-#if defined(CONFIG_CP_STATS)
 	QDF_STATUS status;
+	struct cca_stats cca_stats;
+
 	status = ucfg_mc_cp_stats_cca_stats_get(adapter->vdev, &cca_stats);
 	if (QDF_IS_STATUS_ERROR(status))
 		return -EINVAL;
-#endif
 
 	*congestion = cca_stats.congestion;
 	return 0;
