bf1a69de54be04205965700e090dff22e9f983e0
Najah Muhabbik <abiek2004@gmail.com>
2021-03-13 02:22:25 +0000

    Import scripts
    Signed-off-by: Najah Muhabbik <abiek2004@gmail.com>

---
+++ build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+export KERNELNAME=Moonlight
+
+export LOCALVERSION=Hoshi-v2.2-TEST
+
+export KBUILD_BUILD_USER=frost_id
+
+export KBUILD_BUILD_HOST=droneci
+
+export TOOLCHAIN=clang
+
+export DEVICES=whyred,tulip,lavender,a26x
+
+export CI_ID=-1001463706805
+
+export GROUP_ID=-1001401101008
+
+source helper
+
+gen_toolchain
+
+send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION}..."
+
+send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION}..."
+
+START=$(date +"%s")
+
+for i in ${DEVICES//,/ }
+do
+	build ${i} -oldcam
+
+	build ${i} -newcam
+done
+
+send_msg "⏳ Start building Overclock version..."
+
+send_pesan "⏳ Start building Overclock version..."
+
+git apply oc.patch
+
+git apply em.patch
+
+for i in ${DEVICES//,/ }
+do
+	if [ $i == "whyred" ] || [ $i == "tulip" ]
+	then
+		build ${i} -oldcam -overclock
+
+		build ${i} -newcam -overclock
+	fi
+done
+
+END=$(date +"%s")
+
+DIFF=$(( END - START ))
+
+send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s,get TEST builds in @MoonlightCI | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
+
+send_pesan "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
