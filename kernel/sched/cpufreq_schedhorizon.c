f3087cbb620eeb6b517e1c541030071a8112933b
Prashant1873 <shuklaprashant847@gmail.com>
2021-03-31 00:00:43 +0530

    Edit efficient frequencies

--- kernel/sched/cpufreq_schedhorizon.c
+++ kernel/sched/cpufreq_schedhorizon.c
@@ -29,13 +29,13 @@ unsigned long boosted_cpu_util(int cpu);
 #define cpufreq_enable_fast_switch(x)
 #define cpufreq_disable_fast_switch(x)
 #define UP_RATE_LIMIT_US			(10000)
-#define DOWN_RATE_LIMIT_US			(20000)
+#define DOWN_RATE_LIMIT_US			(10000)
 
-static unsigned int default_efficient_freq_lp[] = {1401600, 1747200};
-static unsigned int default_up_delay_lp[] = {600, 3000};
+static unsigned int default_efficient_freq_lp[] = {1401600, 1536000,1747200};
+static unsigned int default_up_delay_lp[] = {1000, 1000, 3000};
 
-static unsigned int default_efficient_freq_hp[] = {1401600, 1747200, 1958400};
-static unsigned int default_up_delay_hp[] = {600, 1500, 3000};
+static unsigned int default_efficient_freq_hp[] = {1401600, 1747200};
+static unsigned int default_up_delay_hp[] = {4000, 4500};
 
 struct sugov_tunables {
 	struct gov_attr_set attr_set;
