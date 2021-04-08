904de25087c21f4b41abe35db90d2bde0a133e8f
Prashant1873 <shuklaprashant847@gmail.com>
2021-04-08 12:02:50 +0530

    Build Scripts up

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export KERNELNAME=Alyssa
+
+export LOCALVERSION=
+
+export KBUILD_BUILD_USER=Prashant
+
+export KBUILD_BUILD_HOST=kiragod
+
+export TOOLCHAIN=clang
+
+export DEVICES=wayne
+
+source helper
+
+gen_toolchain
+
+send_msg "Building kernel for ${DEVICES}..."
+
+START=$(date +"%s")
+
+for i in ${DEVICES//,/ }
+do 
+
+	build ${i} -Kernel
+
+done
+
+END=$(date +"%s")
+ 
+DIFF=$(( END - START ))
+
+send_msg "BUILD took $((DIFF / 60))m:$((DIFF % 60))s | Most recent changes are : \n $(git log --pretty=format:'%h : %s' -5)"
