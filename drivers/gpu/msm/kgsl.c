7c75bff590d4c6e398c75cb629371f8ab01bf6d7
Sultan Alsawaf <sultan@kerneltoast.com>
2021-08-25 12:59:32 +0530

    msm: kgsl: Affine kgsl_3d0_irq and worker kthread to the big CPU cluster
    These are in the critical path for rendering frames to the display, so
    mark them as performance-critical and affine them to the big CPU
    cluster. They aren't placed onto the prime cluster because the
    single-CPU prime cluster will be used to run the DRM IRQ and kthreads.
    DRM is more latency-critical than KGSL and we need to have DRM and KGSL
    running on separate CPUs for the best performance, so KGSL gets the big
    cluster.
    
    Note that since there are other IRQs requested via kgsl_request_irq(),
    we must specify that the IRQ to be made perf-critical is kgsl_3d0_irq.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [Divyanshu-Modi: adapt for 4.4]
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4767,6 +4767,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 	int status = -EINVAL;
 	struct resource *res;
 	int cpu;
+	unsigned long irqflags = IRQF_TRIGGER_HIGH;
 
 	status = _register_device(device);
 	if (status)
@@ -4853,9 +4854,11 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 		goto error_pwrctrl_close;
 	}
 
+	if (!strcmp(device->name, "kgsl_3d0_irq"))
+		irqflags |= IRQF_PERF_CRITICAL;
+
 	status = devm_request_irq(device->dev, device->pwrctrl.interrupt_num,
-				  kgsl_irq_handler,
-				  IRQF_TRIGGER_HIGH | IRQF_PERF_CRITICAL,
+				  kgsl_irq_handler, irqflags,
 				  device->name, device);
 	if (status) {
 		KGSL_DRV_ERR(device, "request_irq(%d) failed: %d\n",
@@ -5083,13 +5086,15 @@ static int __init kgsl_core_init(void)
 	kgsl_driver.mem_workqueue = alloc_workqueue("kgsl-mementry",
 		WQ_HIGHPRI | WQ_UNBOUND | WQ_MEM_RECLAIM, 0);
 
-	if (IS_ERR_VALUE(kgsl_run_one_worker(&kgsl_driver.worker,
-			&kgsl_driver.worker_thread,
-			"kgsl_worker_thread")) ||
-		IS_ERR_VALUE(kgsl_run_one_worker(&kgsl_driver.low_prio_worker,
-			&kgsl_driver.low_prio_worker_thread,
-			"kgsl_low_prio_worker_thread")))
+	init_kthread_worker(&kgsl_driver.worker);
+
+	kgsl_driver.worker_thread = kthread_run_perf_critical(cpu_perf_mask,
+		kthread_worker_fn, &kgsl_driver.worker, "kgsl_worker_thread");
+
+	if (IS_ERR(kgsl_driver.worker_thread)) {
+		pr_err("unable to start kgsl thread\n");
 		goto err;
+	}
 
 
 	sched_setscheduler(kgsl_driver.worker_thread, SCHED_RR, &param);
